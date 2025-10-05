46ebd72d30867ebf9aef6e42860bdba33a9e4b72
Oschdi <osterjakob@gmail.com>
2025-10-05 19:34:09 +0200

    Basic Setup

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A Nix-flake-based development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        meson
+        cmake
+        ninja
+        python3
+      ];
+
+      buildInputs = with pkgs; [
+        yaml-cpp
+      ];
+
+      shellHook = ''
+        echo "Entered dev env"
+      '';
+    };
+  };
+}
